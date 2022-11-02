23c3dbf66f1a061fab8afabca46b3f28590ae3a4
Ivan <lapchenko.vanya@gmail.com>
2022-11-02 13:32:12 +0200

    Add Domain/DataManager

---
+++ Domain/DataManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.Domain.Repositories.Abstract;
+
+namespace WebApp.Domain
+{
+    public class DataManager
+    {
+        public ITextFieldsRepostory TextFields { get; set; }
+        public IServiceItemsRepository ServiceItems { get; set; }
+
+        public DataManager(ITextFieldsRepostory textFieldsRepostory, IServiceItemsRepository serviceItemsRepository)
+        {
+            TextFields = textFieldsRepostory;
+            ServiceItems = serviceItemsRepository;
+        }
+    }
+}
