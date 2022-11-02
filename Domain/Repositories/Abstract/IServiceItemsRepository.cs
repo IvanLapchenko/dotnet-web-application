9b60bb1f852dd8cc7cec4f076b03ce08674df444
Ivan <lapchenko.vanya@gmail.com>
2022-11-02 13:00:02 +0200

    Add EntityFramework/EF repositories

--- Domain/Repositories/Abstract/IServiceItemsRepository.cs
+++ Domain/Repositories/Abstract/IServiceItemsRepository.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApp.Domain.Entities;
 
 namespace WebApp.Domain.Repositories.Abstract
 {
     public interface IServiceItemsRepository
     {
-
+        IQueryable<ServiceItem> GetServiceItems();
+        ServiceItem GetServiceItemById(Guid id);
+        void SaveServiceItem(ServiceItem entity);
+        void DeleteServiceItem(Guid id);
     }
 }
--- Domain/Repositories/EntityFramework/EFTextFieldsRepostory.cs
+++ Domain/Repositories/EntityFramework/EFTextFieldsRepostory.cs
@@ -2,10 +2,49 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApp.Domain.Repositories.Abstract;
+using WebApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebApp.Domain.Repositories.EntityFramework
 {
-    public class EFTextFieldsRepostory
+    public class EFTextFieldsRepostory : ITextFieldsRepostory
     {
+        private readonly AppDbContext context;
+
+        public EFTextFieldsRepostory(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IQueryable<TextField> GetTextFields()
+        {
+            return context.TextFields;
+        }
+
+        public TextField GetTextFieldById(Guid id)
+        {
+            return context.TextFields.FirstOrDefault(x => x.Id == id);
+        }
+
+        public TextField GetTextFieldByCodeWord(string codeword)
+        {
+            return context.TextFields.FirstOrDefault(x => x.CodeWord == codeword);
+        }
+
+        public void SaveTextField(TextField entity)
+        {
+            if (entity.Id == default)
+                context.Entry(entity).State = EntityState.Added;
+            else
+                context.Entry(entity).State = EntityState.Modified;
+            context.SaveChanges();
+        }
+
+        public void DeleteTextField(Guid id)
+        {
+            context.TextFields.Remove(new TextField() { Id = id });
+            context.SaveChanges();
+        }
     }
 }
