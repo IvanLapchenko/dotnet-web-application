c166448b8d3beed5ad910840438eed1db5d5484e
Ivan <lapchenko.vanya@gmail.com>
2022-11-09 12:07:50 +0200

    Add all the services to main page

---
+++ Models/ViewComponents/WebViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.Domain;
+
+namespace WebApp.Models.ViewComponents
+{
+    public class WebViewComponent : ViewComponent
+    {
+        private readonly DataManager dataManager;
+
+        public WebViewComponent(DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
+
+        public Task<IViewComponentResult> InvokeAsync()
+        {
+            return Task.FromResult((IViewComponentResult)View("Default", dataManager.ServiceItems.GetServiceItems()));
+        }
+    }
+}
