300cc4a356eeecc80e6ade9f2e727c531f16aec6
Ivan <lapchenko.vanya@gmail.com>
2022-11-16 10:33:08 +0200

    Implement Message to admin for admin

---
+++ Areas/Admin/Controllers/MessageController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Domain;
+using System;
+
+namespace WebApp.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MessageController : Controller
+    {
+        private readonly DataManager dataManager;
+        public MessageController(DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
+        public IActionResult Index(Guid id)
+        {
+            if (id != default)
+            {
+                return View("Index", dataManager.Message.GetMessageById(id));
+            }
+
+            return View(dataManager.Message.GetMessages());
+        }
+    }
+}
